8e6f4f10eea3c1ebd212dd3978f9ee60f1a3b7e6
vcu-chengx4 <78166727+vcu-chengx4@users.noreply.github.com>
2022-08-25 12:58:32 -0400

    Delete deployment-status.sh

--- deployment-status.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-if ! kubectl get ns devopslab; then
-    kubectl create ns devopslab
-fi
-
-if ! kubectl rollout status deployment sample-spring-boot -n devopslab; then
-    kubectl apply -f kubernetes.yml
-fi
